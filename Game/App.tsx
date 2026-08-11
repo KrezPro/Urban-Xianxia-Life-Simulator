4e4697d9e94cd1c23c917317b3f6b69cfa647c62
KrezPro <bannov.all@mail.ru>
2026-08-11 15:41:30 +0300

    Принудительная повторная запись корневого App.tsx.

--- Game/App.tsx
+++ Game/App.tsx
@@ -1,7 +1,6 @@
 import { registerRootComponent } from 'expo';
 import App from './src/App';
 
-// Регистрируем компонент принудительно, чтобы Expo точно подхватил наш src/App.tsx
 registerRootComponent(App);
 
 export default App;
