04a79168ed4939942abf5ca6db583a271d0a0579
KrezPro <bannov.all@mail.ru>
2026-08-11 15:39:45 +0300

    Исправление бага точки входа: добавлена явная регистрация registerRootComponent в корневом App.tsx для принудительного рендера нашего UI.

--- Game/App.tsx
+++ Game/App.tsx
@@ -1,3 +1,7 @@
+import { registerRootComponent } from 'expo';
 import App from './src/App';
+
+// Регистрируем компонент принудительно, чтобы Expo точно подхватил наш src/App.tsx
+registerRootComponent(App);
 
 export default App;
